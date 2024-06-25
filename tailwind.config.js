d7a015291db1964b73877db602c834372a4a8c46
tigrouuu <bey.jeremy@gmail.com>
2024-06-25 10:16:28 +0200

    Preline UI Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,15 @@
-/*import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
-/!** @type {import('tailwindcss').Config} *!/
+/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/!*.blade.php',
         './storage/framework/views/!*.php',
-        './resources/views/!**!/!*.blade.php',
-        "./resources/!**!/!*.blade.php",
-        "./resources/!**!/!*.js",
-        "./resources/!**!/!*.vue",
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [forms],
-};*/
-
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './node_modules/preline/dist/*.js',
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
-};
+    plugins: [require('@tailwindcss/forms'), require('preline/plugin')],
+}
